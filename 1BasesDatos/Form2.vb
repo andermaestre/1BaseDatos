4b5cebea296a6c914224ae4466de01d5cc77db2f
Ander Maestre <andermaestre_90@hotmail.es>
2019-11-19 13:30:59 +0100

    Bastante acabado

--- 1BasesDatos/Form2.vb
+++ 1BasesDatos/Form2.vb
@@ -57,4 +57,64 @@ Public Class Form2
 
         Loop While dr.Read
     End Sub
+
+    Private Sub BtnReintegro_Click(sender As Object, e As EventArgs) Handles btnReintegro.Click
+
+
+        If txtImporte.Text.CompareTo("") <> 0 Then
+
+            cn.Open()
+
+            Dim maxNMovimiento As Integer
+            Dim Importe As Double
+            Dim Concepto As String
+            Dim Fecha As Date
+            Dim ncuenta As Integer
+            ncuenta = Integer.Parse(CuentaActual)
+            Importe = Double.Parse(txtImporte.Text)
+            Fecha = Date.Today
+            Concepto = "REINTEGRO CAJERO"
+            consulta.CommandText = String.Format("SELECT MAX(NMovimiento) FROM BaseDatos.Muvimientos")
+            maxNMovimiento = consulta.ExecuteScalar() + 1
+
+            consulta.CommandText = "Insert Into BaseDatos.Muvimientos (NMovimiento, Importe, Concepto, Fecha, NCuenta) Values(@nm,@imp,@con,@fec,@nc)"
+
+            consulta.Parameters.AddWithValue("@nm", maxNMovimiento)
+            consulta.Parameters.AddWithValue("@imp", Importe)
+            consulta.Parameters.AddWithValue("@con", Concepto)
+            consulta.Parameters.AddWithValue("@fec", Fecha)
+            consulta.Parameters.AddWithValue("@nc", ncuenta)
+
+
+
+            consulta.ExecuteNonQuery()
+            cn.Close()
+            cn.Open()
+
+
+            consulta.CommandText = String.Format("SELECT NMovimiento, Importe, Concepto, Fecha FROM BaseDatos.Muvimientos WHERE NCuenta='{0}' ", txtNCuenta.Text)
+            dr = consulta.ExecuteReader()
+
+            If dr.Read Then
+
+                If Not IsDBNull(dr("NMovimiento")) Then
+                    LLenarLista(dr)
+                End If
+
+            End If
+            cn.Close()
+            cn.Open()
+            txtTotalSaldo.Text = Double.Parse(txtTotalSaldo.Text) + Double.Parse(txtImporte.Text)
+            Dim updSaldo As Double
+            updSaldo = Double.Parse(txtTotalSaldo.Text)
+
+            consulta.CommandText = "Insert Into BaseDatos.Cuentas (SaldoActual) Values(@sal)"
+
+            consulta.Parameters.AddWithValue("@sal", updSaldo)
+        Else
+            MsgBox("Caca pa ti")
+        End If
+
+        cn.Close()
+    End Sub
 End Class
